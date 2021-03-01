f377442dc928dee140868296f5072449aab6f651
clonbg <clonbg@gmail.com>
2021-03-01 12:43:11 +0100

    guessing_game loop

--- Readme.md
+++ Readme.md
@@ -31,8 +31,7 @@ cargo check - comprueba
 
 # Por donde voy
 
-Por el punto 2
-Allowing Multiple Guesses with Looping
+file:///home/clonbg/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book/ch02-00-guessing-game-tutorial.html#handling-invalid-input
 
 
 
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,33 +3,37 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-
+    // num aleatorio entre 1 y 100
     let numero_secreto = rand::thread_rng().gen_range(1..101);
 
-    //println!("El número secreto es: {}", numero_secreto);
     
-    let mut numero = String::new();
 
-    println!("Introduce un número: ");
+    loop {
+        // crea string vacío
+        let mut numero = String::new();
+        println!("Introduce un número: ");
 
-    io::stdin()
-        .read_line(&mut numero)
-        .expect("Ha fallado la lectura del numero");
+        // entrada por teclado
+        io::stdin()
+            .read_line(&mut numero)
+            .expect("Ha fallado la lectura del numero");
 
-    let numero: u32 = numero.trim().parse().expect("Please type a number!");
+        // parsea a integer
+        let numero: u32 = numero.trim().parse().expect("Please type a number!");
 
-    println!("Tu número es: {}",numero);
+        println!("Tu número es: {}",numero);
 
-    //pasa a integer i32
-    // let my_i32= numero.parse().unwrap_or(0);
-
-    //println!("{}", type_of(numero));
-    //println!("{}", type_of(&numero_secreto.to_string()));
-
-    match numero.cmp(&numero_secreto) {
-        Ordering::Less => println!("Más grande!!!"),
-        Ordering::Greater => println!("Más pequeño!!!"),
-        Ordering::Equal => println!("Has ganado!!!!!!!!!!!!!!!!!!"),
+        // Compara y ejecuta
+        match numero.cmp(&numero_secreto) {
+            Ordering::Less => println!("Más grande!!!"),
+            Ordering::Greater => println!("Más pequeño!!!"),
+            Ordering::Equal => {
+                println!("Has ganado!!!!!!!!!!!!!!!!!!");
+                break;
+            }
+        }
     }
+
+    
     
 }
